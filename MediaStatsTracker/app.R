f457b0d7534028b836134566f334c0d73f9139f1
eros.dipede <cream_egg@live.com>
2019-05-18 10:37:34 -0400

    Add reactivity to user input

--- MediaStatsTracker/app.R
+++ MediaStatsTracker/app.R
@@ -1,4 +1,4 @@
-library(shiny)
+ library(shiny)
 library(rvest) 
 
 #IMDb - Movies
@@ -12,31 +12,56 @@ igdb <- 'http://www.igdb.com/'
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    #html styles
+    tags$head(
+        tags$style(HTML("
+      h1 {
+        margin-top: 0px;
+      }
+
+    "))
+    ),
     # Application title
     titlePanel("Media Statistics Tracker"),
     tags$hr(),
-    sidebarLayout(
-                  sidebarPanel(
-                    helpText("Discover trends of media mediums throughout the years."),
-                    selectInput("medium", 
-                                label = "Choose a media medium to display:",
-                                choices = list("Film", 
-                                               "Television",
-                                               "Video Games", 
-                                               "Music"),
-                                selected = "Film"),
-                    selectInput(
-                      inputId =  "date", 
-                      label = "Choose a year to display trends from:", 
-                      choices = 1950:2100
-                    )
-                  ),
-                  mainPanel("main panel")
+    sidebarPanel(
+        tags$h1(textOutput("medium")),
+        helpText("Discover trends of media mediums throughout the years."),
+        selectInput("medium", 
+                    label = "Choose a media medium to display:",
+                    choices = list("Film", 
+                                   "Television",
+                                   "Video Games", 
+                                   "Music"),
+                    selected = "Film"),
+        selectInput("graph", 
+                    label = "Choose a graph to display:",
+                    choices = list("Histogram"),
+                    selected = "Histogram"),
+        selectInput(
+          inputId =  "date", 
+          label = "Choose a year to display trends from:", 
+          choices = 1950:2100
+        )
+    ),
+    mainPanel(
+        textOutput("graph"),
+        textOutput("date")
     )
 )
 
 # Define server logic 
-server <- function(input, output) {}
+server <- function(input, output) {
+    output$medium <- renderText({ 
+        paste(input$medium)
+    })
+    output$graph <- renderText({ 
+        paste(input$graph)
+    })
+    output$date <- renderText({ 
+        paste(input$date)
+    })
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
