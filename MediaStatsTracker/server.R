d3ccfa7eff31287357a429f435825d68f11ba13a
ForkBombGIT <erosdipede@gmail.com>
2019-05-31 17:25:40 -0400

    Add datatable, and sidebar data anaylsis

--- MediaStatsTracker/server.R
+++ MediaStatsTracker/server.R
@@ -1,48 +1,72 @@
 library(rvest) 
 library(ggplot2)
+library(DT)
+
 #IMDb - Movies
 imdb_movies <- 'http://www.imdb.com/search/title?count=100&release_date=YEAR,YEAR&title_type=feature'
 
 #IMDb - TV
 imdb_tv <- 'http://www.imdb.com/search/title?count=100&release_date=YEAR,YEAR&title_type=tv_series'
 
-#IGDb - Games
-igdb <- 'https://www.igdb.com/advanced_search'
-
-get.data <- function(year,url) {
+get.data.imdb <- function(year,medium) {
   #format url with year
-  url <- gsub("YEAR",toString(year),url)
+  url <- ifelse(medium == "Film",gsub("YEAR",toString(year),imdb_movies), gsub("YEAR",toString(year),imdb_tv))
   
   #Reading the HTML code from the website
   webpage <- read_html(url)
   
   #grab genre data
+  title_data_html <- html_nodes(webpage,'.lister-item-header a')
   genre_data_html <- html_nodes(webpage,'.genre')
+  rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
+  
+  #title post processing
+  title_data <- gsub("[\r\n]","",html_text(title_data_html))
   
   #genre post processing
   genre_data <- gsub("[ \r\n]","",html_text(genre_data_html))
   genre_data <- gsub(",.*","",genre_data)
   genre_data <- factor(genre_data)
   
+  #rating post processing
+  rating_data <- gsub("[ \r\n]","",html_text(rating_data_html))
+  rating_data <- as.numeric(rating_data)
+  
   #combile all lists to form a dataframe, and return it
-  data.frame(genre = genre_data)
+  data.frame(title = title_data, genre = genre_data,rating = rating_data)
 }
 
 # Define server logic 
-server <- function(input, output) {
+server <- function(input, output) { 
+  #renders the selected medium
   output$medium <- renderText({ 
     paste(input$medium)
   })
+  #renders the selected plot, with selected medium data
   output$plot <- renderPlot({ 
-    df <- NULL
-    if (input$medium == "Film") 
-      df <- get.data(input$date,imdb_movies)
-    else if (input$medium == "Television") 
-      df <- get.data(input$date,imdb_tv)
-    
+    df <- get.data.imdb(input$date,input$medium)
     ggplot(df, aes(x=genre)) + geom_bar()
   })
-  output$date <- renderText({ 
-    paste(input$date)
+  
+  #renders the list of movies
+  output$table <- DT::renderDataTable(get.data.imdb(input$date,input$medium), options = list(scrollX = TRUE))
+  
+  #renders mean of data
+  output$mean <- renderText({
+    df <- get.data.imdb(input$date,input$medium)
+    paste(round(mean(df$rating,rm.na = TRUE),digits = 2))
+  })
+  
+  #renders median of data
+  output$median <- renderText({
+    df <- get.data.imdb(input$date,input$medium)
+    paste(median(df$rating,rm.na = TRUE))
+  })
+  
+  #renders mode of data
+  output$mode <- renderText({
+    df <- get.data.imdb(input$date,input$medium)
+    uvals <- unique(df$rating)
+    paste(uvals[which.max(tabulate(match(df$rating, uvals)))])
   })
 }
--- MediaStatsTracker/ui.R
+++ MediaStatsTracker/ui.R
@@ -1,38 +1,56 @@
 library(ggplot2)
+library(DT)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   #html styles
   tags$head(
     tags$style(HTML("
-      h1 {
+      h3 {
         margin-top: 0px;
+        margin-bottom: 10px;
       }
+      
+      dt { float: left; clear:both; margin-bottom: 5px; font-weight: normal;}
+      dd { float: right; }
+      dl { overflow: hidden; }
     "))
   ),
+  
   # Application title
   titlePanel("Media Statistics Tracker"),
   tags$hr(),
-  sidebarPanel(
-    tags$h1(textOutput("medium")),
-    helpText("Discover trends of media mediums throughout the years."),
-    selectInput("medium", 
-                label = "Choose a media medium to display:",
-                choices = list("Film", 
-                               "Television",
-                               "Video Games", 
-                               "Music"),
-                selected = "Film"),
-    selectInput("graph", 
-                label = "Choose a graph to display:",
-                choices = list("Bar"),
-                selected = "Bar"),
-    selectInput(
-      inputId =  "date", 
-      label = "Choose a year to display trends from:", 
-      choices = 1900:2100
+  sidebarLayout(
+    sidebarPanel(
+      tags$h3(textOutput("medium")),
+      tags$dl(
+        tags$dt("Mean Rating:"),
+        tags$dd(textOutput("mean")),
+        tags$dt("Median Rating:"),
+        tags$dd(textOutput("median")),
+        tags$dt("Mode Rating:"),
+        tags$dd(textOutput("mode"))
+      ),
+      hr(),
+      helpText("Discover trends of media mediums throughout the years."),
+      selectInput("medium", 
+                  label = "Choose a media medium to display:",
+                  choices = list("Film", 
+                                 "Television"),
+                  selected = "Film"),
+      selectInput("graph", 
+                  label = "Choose a graph to display:",
+                  choices = list("Bar"),
+                  selected = "Bar"),
+      selectInput(
+        inputId =  "date", 
+        label = "Choose a year to display trends from:", 
+        choices = 1900:2100
+      )
+    ),
+    mainPanel(
+      plotOutput("plot",height = '450px',width = '550px'),
+      fluidRow(column(DT::dataTableOutput("table"),width = 12))
     )
-  ),
-  mainPanel(
-    plotOutput("plot",height = '450px',width = '575px')
   )
 )
